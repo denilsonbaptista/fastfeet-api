92b63d7959db897d9bf0f7f83a61c17551e4f277
denilson <baptistadenilsonbp@gmail.com>
2025-01-15 21:01:03 -0300

    feat: add the authentication use case

---
+++ src/domain/identity/application/use-cases/authenticate-user.spec.ts
@@ -0,0 +1,44 @@
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
+import { AuthenticateUserUseCase } from './authenticate-user'
+import { makeUser } from 'test/factories/make-user'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+let sut: AuthenticateUserUseCase
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    sut = new AuthenticateUserUseCase(
+      inMemoryUsersRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate a user', async () => {
+    const user = makeUser({
+      name: 'Denis Snow',
+      cpf: '999.999.999-99',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryUsersRepository.items.push(user)
+
+    const result = await sut.execute({
+      cpf: '999.999.999-99',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
---
+++ src/domain/identity/application/use-cases/authenticate-user.ts
@@ -0,0 +1,53 @@
+import { Either, left, right } from '@/core/logic/either'
+import { Encrypter } from '../cryptography/encrypter'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { UsersRepository } from '../repositories/users-repository'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateUserUseCaseRequest {
+  cpf: string
+  password: string
+}
+
+type AuthenticateUserUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+export class AuthenticateUserUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    cpf,
+    password,
+  }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+    const user = await this.usersRepository.findUserByCPF(cpf)
+
+    if (!user) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      user.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: user.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
